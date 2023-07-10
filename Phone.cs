6d1f28e00df161fce6df20362115ae3a2f8fd27b
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2023-07-10 18:21:51 +0300

    Добавьте файлы проекта.

---
+++ Phone.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_ListPhone
+{
+    public class Phone : INotifyPropertyChanged
+    {
+        private string? title;
+        private string? company;
+        private int price;
+
+        public Phone(string title = "Title", string company = "Company", int price = 0)
+        {
+            Title = title;
+            Company = company;
+            Price = price;
+        }
+
+        public string? Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string? Company
+        {
+            get => company;
+            set
+            {
+                company = value;
+                OnPropertyChanged("Company");
+            }
+        }
+        public int Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
